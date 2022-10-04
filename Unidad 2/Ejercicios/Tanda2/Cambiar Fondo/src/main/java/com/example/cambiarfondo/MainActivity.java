d9558007587581a2ee81cde2465cf0024ea68eaa
Alayn Marcos <80107470+Alaynchus@users.noreply.github.com>
2022-10-04 13:19:01 +0200

    Add files via upload

--- Unidad 2/Ejercicios/Tanda2/Cambiar Fondo/src/main/java/com/example/cambiarfondo/MainActivity.java
+++ Unidad 2/Ejercicios/Tanda2/Cambiar Fondo/src/main/java/com/example/cambiarfondo/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.cambiarfondo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    private Button encenderbtn;
+    private Button apagarbtn;
+    private LinearLayout linearLayout;
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        encenderbtn = (Button) findViewById(R.id.encenderbtn);
+        apagarbtn = (Button) findViewById(R.id.apagarbtn);
+        linearLayout = (LinearLayout) findViewById(R.id.linearLay);
+
+        encenderbtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                linearLayout.setBackgroundColor(Color.GREEN);
+            }
+        });
+        apagarbtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                linearLayout.setBackgroundColor(Color.RED);
+            }
+        });
     }
 }
