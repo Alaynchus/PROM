952a13921bf604e996aedfd76662a085345b9eab
Alayn Marcos <80107470+Alaynchus@users.noreply.github.com>
2022-10-11 13:31:17 +0200

    Add files via upload

--- Unidad 2/Ejercicios/Tanda3/src/main/java/com/example/nombreverificar/MainActivity.java
+++ Unidad 2/Ejercicios/Tanda3/src/main/java/com/example/nombreverificar/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.nombreverificar;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText nombrelbl;
+    private EditText apellidoslbl;
+    private EditText condicioneslbl;
+    private Button verificarbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nombrelbl = findViewById(R.id.nombrelbl);
+        apellidoslbl = findViewById(R.id.apellidolbl);
+        condicioneslbl = findViewById(R.id.resultadolbl);
+
+
     }
+    public void listerverificarbtn(View view){
+        String nom = nombrelbl.getText().toString();
+        String ape = apellidoslbl.getText().toString();
+
+        Intent intent = new Intent(MainActivity.this, AceptarActivity.class);
+        intent.putExtra("nombre",nom);
+        intent.putExtra("apellidos",ape);
+        startActivityForResult(intent,1);
+    }
+
+
 }
