7c8c49527557ed901318d2d5c879ae57e958cd31
Alayn Marcos <80107470+Alaynchus@users.noreply.github.com>
2022-10-17 12:33:49 +0200

    Add files via upload

--- Unidad 2/Ejercicios/Tanda3/CalculadoraJuego/src/main/java/com/example/calculadorajuego/MainActivity.java
+++ Unidad 2/Ejercicios/Tanda3/CalculadoraJuego/src/main/java/com/example/calculadorajuego/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.calculadorajuego;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText num1, num2, numCorrectas, numIncorrectas;
+    private EditText   resultadotxt;
+    private TextView numCorrectas, numIncorrectas, num1, num2;
+    private int nu1, nu2, nuCorrectas, nuIncorrectas;
 
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +22,54 @@ public class MainActivity extends AppCompatActivity {
 
         num1 = findViewById(R.id.numero1txt);
         num2 = findViewById(R.id.numero2txt);
+        numCorrectas = findViewById(R.id.correctaslbl);
+        numIncorrectas = findViewById(R.id.incorrectaslbl);
+        resultadotxt = findViewById(R.id.resultadotxt);
+        nu1=0;
+        nu2=0;
+        nuCorrectas=0;
+        nuIncorrectas=0;
         crearNumerosRandom();
     }
     private void crearNumerosRandom(){
-        num1.setText(3+"");
-        num2.setText(4+"");
 
+        nu1=(int)(Math.random()*100);
+        num1.setText(String.valueOf(nu1));
+        nu2=(int)(Math.random()*100);
+        num2.setText(String.valueOf(nu2));
+
+
+    }
+    public void comprobarListenerBtn(View view) {
+        try {
+            int resulcom = Integer.parseInt(resultadotxt.getText().toString());
+            Intent intent = new Intent(MainActivity.this, ResultadoOperacionActivity.class);
+            intent.putExtra("nu1",nu1);
+            intent.putExtra("nu2",nu2);
+            intent.putExtra("resulcom",resulcom);
+            startActivityForResult(intent,2);
+        }
+        catch(NumberFormatException e) {
+            //no es numeríco
+        }
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 2 && resultCode==RESULT_OK){
+            crearNumerosRandom();
+            resultadotxt.setText("");
+
+            boolean respuesta = data.getExtras().getBoolean("esCorrecta");
+            if(respuesta){
+                nuCorrectas++;
+            }
+            else{
+                nuIncorrectas++;
+            }
+            numCorrectas.setText(nuCorrectas+"");
+            numIncorrectas.setText(nuIncorrectas+"");
+        }
     }
 }
--- Unidad 2/Ejercicios/Tanda3/CalculadoraJuego/src/main/java/com/example/calculadorajuego/ResultadoOperacionActivity.java
+++ Unidad 2/Ejercicios/Tanda3/CalculadoraJuego/src/main/java/com/example/calculadorajuego/ResultadoOperacionActivity.java
@@ -1,8 +1,9 @@
 package com.example.calculadorajuego;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.EditText;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -10,13 +11,43 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class ResultadoOperacionActivity extends AppCompatActivity {
 
+    private TextView respresulbl;
+    private Boolean esCorrecta;
 
-
-    @SuppressLint("MissingInflatedId")
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado_operacion);
 
+        respresulbl= findViewById(R.id.respuestaresultadolbl);
+        esCorrecta=acertada();
+        compresul();
+    }
+
+    public boolean acertada(){
+        Bundle datos = getIntent().getExtras();
+        int nu1 = datos.getInt("nu1");
+        int nu2 = datos.getInt("nu2");
+        int resulcom = datos.getInt("resulcom");
+        if(nu1+nu2==resulcom)
+            return true;
+        else
+            return false;
+    }
+
+    public void compresul(){
+        if(acertada()==true){
+            respresulbl.setText("El resultado de la operacion es CORRECTA");
+        }
+        else{
+            respresulbl.setText("El resultado de la operacion es INCORRECTA");
+        }
+    }
+
+    public void listenerVorlverbtn(View view){
+        Intent intent= new Intent();
+        intent.putExtra("esCorrecta", esCorrecta);
+        setResult(RESULT_OK, intent);
+        finish();
     }
 
 }
